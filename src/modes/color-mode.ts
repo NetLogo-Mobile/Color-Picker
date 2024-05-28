856d925e2bb434dc090da61937df924bc9475e9c
John Chen <john@soobb.com>
2024-05-27 19:38:13 -0500

    Very minor refactoring

--- src/modes/color-mode.ts
+++ src/modes/color-mode.ts
@@ -9,13 +9,15 @@ type ColorPickerState = {
 };
 
 /** ColorMode: Base class for each of the ColorPicker Modes */
-class ColorMode {
+abstract class ColorMode {
     // A ColorMode needs to know the ColorPicker state for initial values 
     public state: ColorPickerState;
     // parent: the HTML element in which the ColorMode will add its body to
     public parent: HTMLElement;
     // setState: a function to change the state of the ColorPicker, passed from the ColorPicker
     public setState: (newState: Partial<ColorPickerState>) => void;
+
+    /** constructor: sets the parent, state, and setState */
     constructor(parent: HTMLElement, state: ColorPickerState, setState: (newState: Partial<ColorPickerState>) => void){
         this.parent = parent;
         this.state = state;
@@ -23,19 +25,11 @@ class ColorMode {
     }
 
     /** toDOM: A mode of the colorPicker will change the body of the color picker*/
-    public toDOM() : void{
-        throw new Error("Method not implemented.");
-    }
-
+    public abstract toDOM() : void;
     /** showNumbers: will show numbers in the mode */
-    public showNumbers() : void {
-        throw new Error("Method not implemented.");
-    }
-
+    public abstract showNumbers() : void;
     /** hideNumbers: nhide the numbers in the ColorPicker preview */
-    public hideNumbers() : void {
-        throw new Error("Method not implemented.");
-    }
+    public abstract hideNumbers() : void;
 }
 
 export { ColorMode, ColorPickerState };
